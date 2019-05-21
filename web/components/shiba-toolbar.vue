7c815dc5586f7363c24355ca50be6d29d522e19a
唐龙浩 <longhao.tang@shenmajr.com>
2019-05-21 15:14:25 +0800

    完成前端框架布局

---
+++ web/components/shiba-toolbar.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <v-toolbar dense dark app fixed color="primary" :clipped-left="true">
+    <v-toolbar-items style="width: 203px;">
+      <v-btn block :ripple="false" flat style="margin-right: 24px;">
+        <v-icon>rowing</v-icon>
+        <v-toolbar-title>Shiba</v-toolbar-title>
+      </v-btn>
+    </v-toolbar-items>
+    <v-toolbar-items>
+      <v-btn flat to="/">主页
+        <v-icon right>keyboard_arrow_down</v-icon>
+      </v-btn>
+      <v-btn flat to="/project">项目
+        <v-icon right>keyboard_arrow_down</v-icon>
+      </v-btn>
+      <v-btn flat to="/user">用户
+        <v-icon right>keyboard_arrow_down</v-icon>
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat>
+        <v-avatar @click="" size="30" color="indigo">
+          <v-icon dark>account_circle</v-icon>
+        </v-avatar>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {}
+</script>
---
+++ web/pages/project/_key/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>djfakl</div>
+</template>
+
+<script>
+  export default {
+    layout: "project"
+  }
+</script>
