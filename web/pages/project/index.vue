cf9af909381b30ef869455933a003482acdec58a
唐龙浩 <longhao.tang@shenmajr.com>
2019-05-21 18:22:34 +0800

    拆分部分页面，新增login页面

---
+++ web/pages/project/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>项目列表
+  <v-btn to="/project/ceshi">测试</v-btn></div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        result: null
+      }
+    },
+    mounted() {
+      this.$axios.get("/api/project/").then(({data}) => {
+        this.result = data;
+      })
+    }
+  }
+</script>
